2424a795ea990e1ff2a8204ea00a691dc537a3a9
beechd <beechd94@gmail.com>
2020-03-07 19:56:14 -0500

    Added getSize(), add(), and print() methods for Generic Array

--- DynamicArray/src/GenericArray.java
+++ DynamicArray/src/GenericArray.java
@@ -1,6 +1,8 @@
 
 public class GenericArray
 {
+	private int[] elements;
+
 	private int capacity;
 	private int size;
 
@@ -8,5 +10,35 @@ public class GenericArray
 	{
 		size = 0;
 		capacity = 10;
+		elements = new int[capacity];
+	}
+
+	public int getSize()
+	{
+		return size;
+	}
+
+	public void add(int element)
+	{
+		if (capacity == size)
+		{
+			capacity *= 2;
+			int[] temp = new int[capacity];
+			for (int i = 0; i < size; i++)
+			{
+				temp[i] = elements[i];
+			}
+			elements = temp;
+		}
+		elements[size++] = element;
+	}
+	
+	public void print()
+	{
+		for (int element : elements)
+		{
+			System.out.print(element + " ");
+		}
+		System.out.println();
 	}
 }
